18a426a1d2559c0208ba6ac0c5adf02b76ce52b0
1Door <2753652621@qq.com>
2022-05-11 17:05:50 +0800

    first time

---
+++ assignment8/CGL/tests/build/CMakeFiles/osd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/osd.dir/osd.cpp.o"
+  "osd.pdb"
+  "osd"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/osd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
